ff2fdf1201a43ac5ac9ec7a9f942355541b5d800
sveta-lametta <89455586+sveta-lametta@users.noreply.github.com>
2025-06-30 10:52:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-import os
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет! Я работаю 👋")
 
-def main():
+async def main():
     application = ApplicationBuilder().token(os.getenv("BOT_TOKEN")).build()
     application.add_handler(CommandHandler("start", start))
-    application.run_polling()
+    print("Бот запускается...")  # Для Railway логов
+    await application.run_polling()
 
 if __name__ == '__main__':
-    main()
+    import asyncio
+    asyncio.run(main())
